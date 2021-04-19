d9ae8970ada2c08e55e6d25620eea7c4d05e54e1
Arnaldo Aguiar <arnaldoaguiarp@gmail.com>
2021-04-19 20:35:19 -0300

    Criando o modulo USERS

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [TasksModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
